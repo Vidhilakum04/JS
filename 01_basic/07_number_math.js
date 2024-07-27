c3df08e5fda804c4c8cdc59b26fb26143ce6b451
Vidhilakum04 <156897236+Vidhilakum04@users.noreply.github.com>
2024-07-27 10:18:35 +0000

    number_math

---
+++ 01_basic/07_number_math.js
@@ -0,0 +1,37 @@
+const score = 400
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.2345
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN')); // india localstring
+
+
+
+//+++++++++ maths ++++++++++
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // absolute value
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3)); // ceiling value
+// console.log(Math.floor(4.9)); // floor value
+// console.log(Math.min(4,3,5,8));
+// console.log(Math.mix(4,3,1,2));
+
+console.log(Math.random()); // its always give us output in beetween 0 and 1
+console.log((Math.random()*10) + 1); // its gives us in 1 to 9
+console.log((Math.random()*10) + 1);
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
+
