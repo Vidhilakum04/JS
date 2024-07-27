ad24af64409038472ebdbc165be5ff1bf649817f
Vidhilakum04 <156897236+Vidhilakum04@users.noreply.github.com>
2024-07-27 17:53:29 +0000

    Json talk

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -17,7 +17,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
 
 // console.log(regularUser.fullname?.userfullname.firstname);
 //if we have this ? sign ==> then we get as similar as to the if-else use
@@ -29,7 +29,7 @@ const obj2={3:"c", 4:"d"}
 // const obj3 = Object.assign({},obj1,obj2) // {} ==> use as a target for empty object , then we get output in empty object , its not compulsory , but better than without{}.
 
 const obj3={...obj1,...obj2}
-console.log(obj3);
+// console.log(obj3);
 
 const users=[
     {
@@ -42,13 +42,46 @@ const users=[
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggerIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggerIn'));
 
+
+// ******** De - Structuring *******
+
+const course = {
+    coursename :" js in hindi",
+    price:"999",
+    courseInstructor:"vidhi"
+}
+
+// course.courseInstructor
+
+const{courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+//******
+// const navbar = ({company}) =>{ // how to de-structure objects
+    
+// }
+// navbar(company = "vidhi")
+
+// {
+//     "name": ' vidhi',        // **** its JSON syntax *****
+//     "courseName": "js ",
+//     "price":"free"
+
+// }
+
+ [   //*** also JSON format ******/
+    {},
+    {},
+    {}
+ ]
 
 
